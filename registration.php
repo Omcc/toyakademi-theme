fa14e6f12e0dcd03331592ca5c55140bec407e79
yunus <yunusyuksel95@gmail.com>
2021-06-28 23:34:15 +0300

    changes

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Hiddentechies/bizkick',
+    'frontend/Swe/toyakademi',
     __DIR__
 );
